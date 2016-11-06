6dac809079dbafb87ac4282365ce81487b56c4f8
Martijn Courteaux <courteauxmartijn@gmail.com>
2016-11-06 02:15:00 +0100

    Fixed two bugs.

--- Camera.cpp
+++ Camera.cpp
@@ -9,7 +9,7 @@ Camera::Camera(glm::vec3 position, glm::vec3 target, glm::vec3 up,int shaderProg
     mTarget = target;
     mPosition = position;
     mShaderProgramID = shaderProgramID;
-    glm::vec4 LookDirection = glm::vec4(mTarget - mPosition, 0.0f);
+    glm::vec3 LookDirection = mTarget - mPosition;
     mLookDir = normalize(LookDirection);
     update();
 }
@@ -42,7 +42,7 @@ void Camera::update() {
 
 void Camera::rotate(float angle) {
     glm::vec4 LookDirection = glm::vec4(mTarget - mPosition,0.0f);
-    mLookDir = normalize(LookDirection);
+    mLookDir = normalize(glm::vec3(LookDirection));
     //rotate around y axis
     glm::mat4 rotationMat = glm::rotate(glm::mat4(1), angle, glm::vec3(0, 1, 0));
     glm::vec3 rotated = glm::vec3(rotationMat*LookDirection); //discard the w comp;
